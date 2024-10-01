cae1c5c60bef9cd7b96e9255c57207d9717e0273
Johannes Hicham B. <hbougdal@protonmail.com>
2024-10-01 11:51:55 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+#Gemini details
+GEMINI_API_KEY = "REPLACE_GEMINI_API_KEY" #Replace
+GEMINI_MODEL_TYPEs = ["gemini-1.5-flash"] 
+
+#WooCommerce Store details
+STORE_URL = "e.g. https://mystore.com/"  #Replace
+CONSUMER_KEY = "WooCommerce_CONSUMER_KEY" #Replace
+CONSUMER_SECRET = "WooCommerce_CONSUMER_SECRET" #Replace
+
+# Output files for persistence
+PRODUCTS_FILE_NAME = "products.json"
+PAGES_FILE_NAME    = "pages.json"
+LOGS_FILE          = "logs.json"
